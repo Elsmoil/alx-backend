a5cdfdaf9ee076a9df05f51d93927c91bfed9ec0
Elsmoil <alsmoilalrasta999@gmail.com>
2024-12-05 17:19:12 +0000

    nice

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -12,3 +12,24 @@ import redis from 'redis';
    client.on('error', (err) => {
      console.log(`Redis client not connected to the server: ${err.message}`);
      });
+// Function to set a new school value
+function setNewSchool(schoolName, value, callback) {
+client.set(schoolName, value, redis.print); // Store the value in Redis with the key `schoolName`
+ if (callback) callback(); // Call the callback after setting the value
+   }
+
+ // Function to display the value of a school
+function displaySchoolValue(schoolName) {
+       client.get(schoolName, (err, reply) => {
+           if (err) {
+console.log(`Error retrieving the value for ${schoolName}: ${err}`);
+                     } else {
+         console.log(`${schoolName}: ${reply}`);
+                               }
+                                 });
+                                 }
+      // Call the functions as specified
+ displaySchoolValue('ALX'); // This will show the value for the ALX key
+ setNewSchool('ALXSanFrancisco', '100', () => {
+            displaySchoolValue('ALXSanFrancisco'); // Display value after setting the new value for ALXSanFrancisco.
+ });
